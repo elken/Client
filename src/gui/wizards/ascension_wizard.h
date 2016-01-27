9fe8ce6d15b837a66936a7eecaf5c2908afd6963
Ellis Kenyo <elken@mykolab.com>
2016-01-27 15:57:02 +0000

    Wizard init

--- src/gui/wizards/ascension_wizard.h
+++ src/gui/wizards/ascension_wizard.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <QWizard>
+#include <QSettings>
 
-class ascension_wizard
+class AscensionWizard : public QWizard
 {
+    Q_OBJECT
 
+public:
+    AscensionWizard(QString title = "Project Ascension", QWidget* parent = 0);
+    ~AscensionWizard();
+    
 };
 
 
